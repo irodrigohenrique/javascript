90c68e382f61d1b076c7f58740253f0e524da87a
rodrihenriqe <100159330+rodrihenriqe@users.noreply.github.com>
2022-10-18 17:43:44 -0300

    fazendo novos exercicios

---
+++ ex005/index.js
@@ -0,0 +1,11 @@
+var vel =  80
+
+console.log(`A velocidade do seu carro ${vel} Km/h`)
+
+
+if (vel > 60){ // CONDIÇÃO SIMPLES!
+        console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+
+console.log(`Dirija sempre usando cinto de segurança!`)
